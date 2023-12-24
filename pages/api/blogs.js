4d5dfa6e79d9e7761920707f8599df9e0318eb60
Aman Kumar Mishra <aman@Amans-MacBook-Air.local>
2023-12-24 12:57:28 +0530

    added blogs api to blog page

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,9 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from 'fs';
-export default function handler(req, res) {
-  fs.readdir("blogdata", (err, data) => {
-    console.log(data)
-    res.status(200).json(data)
-  })
-
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir("blogdata")
+  let myFile;
+  let allBlogs = []
+  for (let i = 0; i < data.length; i++) {
+    const item = data
+    console.log(item)
+    myFile = await fs.promises.readFile("blogdata/" + data[i], "utf-8")
+    allBlogs.push(JSON.parse(myFile))
+  }
+  {/*
+    data.forEach((item) => {
+      fs.readFile("blogdata/" + item, "utf-8", (d) => {
+        allBlogs.push(d)
+      })
+    })
+    res.status(200).json(allBlogs)
+  })*/}
+  res.status(200).json(allBlogs)
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link';
 
 // Step 1: Collect all the files from blogdata directory
 // Step 2: Iterate through and display them
 const Blog = () => {
+  const [blogs, setBlogs] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:3000/api/blogs")
+      .then(a => {
+        return a.json()
+      })
+      .then(parsed => setBlogs(parsed))
+  }, [])
   return <div className={styles.container}>
     <main className={styles.main}>
-      <div className={styles.blogItem}>
-        <Link href={"/blogpost/learn-javascript"}>
-          <h3>How to learn JavaScript in 2022?</h3></Link>
-        <p>JavaScript is the language used to design logic for the web</p>
-      </div>
-      <div className="blogItem">
-        <h3>How to learn JavaScript in 2022?</h3>
-        <p>JavaScript is the language used to design logic for the web</p>
-      </div>
-      <div className="blogItem">
-        <h3>How to learn JavaScript in 2022?</h3>
-        <p>JavaScript is the language used to design logic for the web</p>
-      </div>
+      {
+        blogs.map(blogItem => {
+          return (
+            <div key={blogItem.slug} className={styles.blogItem}>
+              <Link href={`/blogpost/${blogItem.slug}`}>
+                <h3 className={styles.blogItemh3}>{blogItem.title}</h3></Link>
+              <p className={styles.blogItemp}>{blogItem.content.substr(0, 140)}...</p>
+            </div>
+          )
+        })
+      }
+
     </main>
   </div>
 };
