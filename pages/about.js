56fa1f7a760d418480d224b1c8cf0ed9f9c5a5f7
Aman Kumar Mishra <aman@Amans-MacBook-Air.local>
2024-02-27 01:48:11 +0530

    added about pagr

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import styles from '../styles/About.module.css'
 
 const About = () => {
-  return <div>This is my about component</div>;
+  return <div className={styles.container}>
+    <h1 className={styles.center}>About Hunting Coder</h1>
+    <h2>Introduction</h2>
+    <p>Welcome to [Your Blog Name] – a digital space where knowledge meets inspiration, and curiosity finds its answers. Our blog is designed to be your go-to source for insightful, engaging, and diverse content that caters to a broad spectrum of interests.
+
+      At [Your Blog Name], we believe that knowledge is a powerful tool that can transform lives. With this philosophy at our core, we curate content that spans various topics, ensuring there's something for everyone. Whether you're a tech enthusiast, a travel buff, a wellness seeker, or a literature lover, our blog is your virtual haven.
+
+      Dive into the realms of technology with our in-depth articles on the latest trends, innovations, and breakthroughs. From gadget reviews to explorations of emerging technologies, we keep you at the forefront of the digital landscape. Stay informed and inspired as we unravel the complexities of the tech world in a way that's accessible to all.</p>
+    <h2>Services Offered</h2>
+    <p>
+      Welcome to [Your Blog Name] – a digital space where knowledge meets inspiration, and curiosity finds its answers. Our blog is designed to be your go-to source for insightful, engaging, and diverse content that caters to a broad spectrum of interests.
+
+      At [Your Blog Name], we believe that knowledge is a powerful tool that can transform lives. With this philosophy at our core, we curate content that spans various topics, ensuring there's something for everyone. Whether you're a tech enthusiast, a travel buff, a wellness seeker, or a literature lover, our blog is your virtual haven.
+
+      Dive into the realms of technology with our in-depth articles on the latest trends, innovations, and breakthroughs. From gadget reviews to explorations of emerging technologies, we keep you at the forefront of the digital landscape. Stay informed and inspired as we unravel the complexities of the tech world in a way that's accessible to all.
+    </p>
+    <p>We offer the following services</p>
+    <ul>
+      <li>Service 1</li>
+      <li>Service 1</li>
+      <li>Service 1</li>
+      <li>Service 1</li>
+      <li>Service 1</li>
+      <li>Service 1</li>
+      <li>Service 1</li>
+    </ul>
+    <h2>Contact us</h2>
+    <p>
+      Welcome to [Your Blog Name] – a digital space where knowledge meets inspiration, and curiosity finds its answers. Our blog is designed to be your go-to source for insightful, engaging, and diverse content that caters to a broad spectrum of interests.
+
+      At [Your Blog Name], we believe that knowledge is a powerful tool that can transform lives. With this philosophy at our core, we curate content that spans various topics, ensuring there's something for everyone. Whether you're a tech enthusiast, a travel buff, a wellness seeker, or a literature lover, our blog is your virtual haven.
+
+      Dive into the realms of technology with our in-depth articles on the latest trends, innovations, and breakthroughs. From gadget reviews to explorations of emerging technologies, we keep you at the forefront of the digital landscape. Stay informed and inspired as we unravel the complexities of the tech world in a way that's accessible to all.
+    </p>
+  </div>;
 };
 
 export default About;
